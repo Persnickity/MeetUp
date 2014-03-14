db7c1c13c7ea92aa8a62716c4255ff8e36fe09d2
jeffrey <jr926@drexel.edu>
2014-03-14 13:50:44 -0400

    General Layout with buttons and onClick methods+GPS working
    Signed-off-by: jeffrey <jr926@drexel.edu>

--- MeetUP/src/com/wdb32/meetup/MainActivity.java
+++ MeetUP/src/com/wdb32/meetup/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -17,7 +18,8 @@ public class MainActivity extends Activity {
 	String rootCause = "Main Activity";
 	double lat, lon;
 	TextView view;
-	Button getGPS;
+	Button updateGPSD, createEvent, editEvent, createGroup, addToGroup,
+			Checkin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +37,55 @@ public class MainActivity extends Activity {
 	}
 
 	public void doThis() {
-		getGPS = (Button) findViewById(R.id.gpsButton);
+		TelephonyManager tMgr = (TelephonyManager) this
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		String myPhoneNumber = tMgr.getLine1Number();
+		Log.i(rootCause, myPhoneNumber);
 		view = (TextView) findViewById(R.id.gpstemp);
-		getGPS.setOnClickListener(new View.OnClickListener() {
+		linkButtons();
+	}
 
+	public void linkButtons() {
+		updateGPSD = (Button) findViewById(R.id.gpsButton);
+		createEvent = (Button) findViewById(R.id.createEvent);
+		editEvent = (Button) findViewById(R.id.editEvent);
+		createGroup = (Button) findViewById(R.id.createGroup);
+		addToGroup = (Button) findViewById(R.id.addToGroup);
+
+		buttonsOnClickSet();
+	}
+
+	private void buttonsOnClickSet() {
+		updateGPSD.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {// getting location in button click
-											// then update display
-				LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-				List<String> providers = lm.getProviders(true);
-				Location l = null;
-				for (int i = providers.size() - 1; i >= 0; i--) {
-					l = lm.getLastKnownLocation(providers.get(i));
-					if (l != null)
-						break;
-				}
-				if (l != null) {
-					lat = l.getLatitude();
-					lon = l.getLongitude();
-				}
+				updateGPS(); // then update display
 				view.setText("lat:" + lat + "\nlon" + lon);
 				view.invalidate();
 			}
 		});
+		createEvent.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			}
+		});
+		editEvent.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			}
+		});
+		createGroup.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 
+			}
+		});
+		addToGroup.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
 	}
 
 	public double calculateMiles(double latitude, double longitude) {
@@ -84,7 +111,18 @@ public class MainActivity extends Activity {
 		return Math.sqrt(dx + dy);
 	}
 
-	public void getGPS() {
-
+	public void updateGPS() {
+		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		List<String> providers = lm.getProviders(true);
+		Location l = null;
+		for (int i = providers.size() - 1; i >= 0; i--) {
+			l = lm.getLastKnownLocation(providers.get(i));
+			if (l != null)
+				break;
+		}
+		if (l != null) {
+			lat = l.getLatitude();
+			lon = l.getLongitude();
+		}
 	}
 }
